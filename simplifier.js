09ca60584d3381a47d599f0003448003a38f40a2
b-bly <brendt.bly@outlook.com>
2023-06-06 08:35:28 -0500

    Simplify multiple nested parenthesis.

--- simplifier.js
+++ simplifier.js
@@ -2,13 +2,11 @@
 
 // look for innermost parenthesis
 
-// add
+// add terms (positive and negative terms)
 // split into array of + or - terms?
 // add terms with same var
 // sort into object with keys of each var (ignore multivar terms at first)
 // add
-
-// subtract
 
 // multiply (including negatives)
 // distribute multiplier 2(x+1)
@@ -18,123 +16,207 @@
 // substitue var from eqation
 
 // solve for var
-// eval parenthesis
-// if outside parenthesis
-// add/subtract from both sides
+// eval parenthesis, combine terms
+// if outside parenthesis, distribute
+// add/subtract from both sides of eq
 // divide/multiply by constant
 
 function onlyUnique(value, index, array) {
-  return array.indexOf(value) === index;
+    return array.indexOf(value) === index
 }
-const left = /(.*)=.*/;
-const right = /.*=(.*)/;
-const innermostParenthesis = /\(((?:"\(|\)"|[^()])+)\)/;
+
+const innermostParenthesis = /(?:\d*)\(((?:"\(|\)"|[^()])+)\)/ // group 1: coeff group 2: expression
+const innermostParenthesisWithCoeff = /(\d+)\((?:"\(|\)"|[^()])+\)/ // group 1: coeff group 2: expression
+const innermostParenthesisAndSign = /(?:[+-]\d+)?(?:\d+)?\((?:"\(|\)"|[^()])+\)/ // /(?:[+-])?(?:\d+)?\((?:"\(|\)"|[^()])+\)/;
 
 class Simplifier {
-  constructor(equalities, formula) {
-    this.equalities = equalities.map((equality) => equality.trim());
-    this.formula = formula.trim();
-  }
-
-  /**
-   * Split into an array of + and - terms, ie: [-x, 3y, z]
-   * @param {string} expression
-   */
-  splitExpression(expression) {
-    const keepSign = /(?=[-+])/g
-    return expression.split(keepSign); // (/(?!^)-|\+/)
-  }
-
-  /**
-   * returns an array of variables in the expression [2x, -3y, y] => [x, y]
-   * @param {array} expressionArray array returned by splitExpression
-   * This does not handle terms with multiple variables
-   */
-  getVariables(expressionArray) {
-    return expressionArray.map((term) => term.match(/[a-zA-Z]{1}/)[0]).filter(onlyUnique);
-  }
-
-  getRightSide(expression) {
-    return expression.match(right)[1];
-  }
-
-  getLeftSide(expression) {
-    return expression.match(left)[1];
-  }
-
-  getInnerMostParenthesis(expression) {
-    return expression.match(innermostParenthesis);
-  }
-
-  // need to refactor to handle * and / terms
-  add(expression) {
-    const expressionArray = this.splitExpression(expression);
-    const variables = this.getVariables(expressionArray);
-    return variables
-      .map((variable, i) => {
-        // get terms containing variable
-        let terms = expressionArray.filter((expression) =>
-          expression.includes(variable)
-        );
-        const coefficientGroup = terms.map((term) => {
-          let sign = "";
-          if (/-/.test(term)) {
-            sign = "-";
-          }
-          let value = 1;
-          if (/\d/.test(term) === true) {
-            value = term.match(/\d+/)[0];
-          }
-          return parseInt(sign + value);
-        });
-        const coefficientSum = coefficientGroup.reduce(
-          (acc, number) => (acc += number),
-          0
-        );
-        let term = coefficientSum.toString() + variable;
-        if (coefficientSum === 0) { 
-            term = '' 
-        } else if (coefficientSum === 1) { 
-            term = variable 
-        } else if (coefficientSum === -1) { 
-            term = '-' + variable 
-        }
-        if (!/-/.test(term) && i !== 0) { term = '+' + term }
-        return term
-      })
-      .join("");
-  }
-
-  getVariable(term) {
-    return term.match(/[a-zA-Z]+/)[0];
-  }
-
-  /**
-   * returns an array of two terms
-   * @param {string} expression simple expression like 2a+b
-   * @param {string} symbol \\+ for example.  Symbol that separates the terms
-   */
-  getTerms(expression, symbol) {
-    const termsRegex = new RegExp(`(.+)${symbol}(.+)`);
-    const termsMatch = expression.match(termsRegex);
-    const left = termsMatch[1];
-    const right = termsMatch[2];
-    return [left, right];
-  }
-
-  getCoefficient(term) {
-    let numberMatch = term.match(/\d+/);
-    const negative = /^-/.test(term);
-    let number = 1;
-    if (numberMatch !== null) {
-      number = numberMatch[0];
+    constructor(equalities, formula) {
+        this.equalities = equalities.map((equality) => equality.trim())
+        this.formula = formula.trim()
     }
-    return negative ? parseInt("-" + number) : parseInt(number);
-  }
+
+    // need to refactor to handle * and / terms
+    add(expression) {
+        const expressionArray = this.splitExpression(expression)
+        const variables = this.getVariables(expressionArray)
+        return variables
+            .map((variable, i) => {
+                // get terms containing variable
+                let terms = expressionArray.filter((expression) => expression.includes(variable))
+                const coefficientGroup = terms.map((term) => {
+                    return this.parseCoefficient(term)
+                })
+                const coefficientSum = coefficientGroup.reduce((acc, number) => (acc += number), 0)
+                const term = this.formatCoefficient(coefficientSum, variable, i)
+                return term
+            })
+            .join('')
+    }
+
+    // position is order in the equation.  0 = 1st.
+    // a + b // a is position 0, b position 1
+    formatCoefficient(coefficientSum, variable, position) {
+        let term = coefficientSum.toString() + variable
+        if (coefficientSum === 0) {
+            term = ''
+        } else if (coefficientSum === 1) {
+            term = variable
+        } else if (coefficientSum === -1) {
+            term = '-' + variable
+        }
+        if (!/-/.test(term) && position !== 0) {
+            term = '+' + term
+        }
+        return term
+    }
+
+    /**
+     * Distribute multiplier to parenthesis and add like terms
+     * @param {number} multiplier
+     * @param {string} expression 2a-1
+     */
+    simplify(expression) {
+        // while still parenthesis, add
+        if (this.hasParenthesis(expression)) {
+            // evaluate innermost parenthesis
+            const evaluatedExpression = this.evaluateParenthesis(expression)
+            expression = this.replaceParentheticalExpression(expression, evaluatedExpression)
+            console.log(expression)
+            if (innermostParenthesis.test(expression) === true) {
+                return this.simplify(expression)
+            }
+        }
+        // final adding terms if needed
+        return this.add(expression)
+    }
+
+    replaceParentheticalExpression(expression, evaluatedExpression) {
+        // add + if no negative and there is no term to the left
+        // 3x+2(2y), 4y => 3x+4y
+        // 2(2y), 47 => 4y
+        if (innermostParenthesis.test(expression) === true && !/^\-/.test(evaluatedExpression)) {
+            evaluatedExpression = '+' + evaluatedExpression
+        }
+        expression = expression.replace(innermostParenthesisAndSign, evaluatedExpression)
+        return expression
+    }
+
+    hasParenthesis(expression) {
+        return /\(|\)/.test(expression)
+    }
+
+    getInnerMostParenthesis(expression) {
+        const match = expression.match(innermostParenthesis)
+        const coeff = expression.match(innermostParenthesisWithCoeff)
+        console.log('coeff', coeff)
+        if (coeff?.length > 1) {
+            return [coeff[1], match[1]]
+        }
+        return [1, match[1]]
+    }
+
+    // Distribute, add
+    evaluateParenthesis(expression) {
+        const [parenthesisCoefficient, innerExpression] = this.getInnerMostParenthesis(expression)
+        const simplifiedExpression = this.add(innerExpression)
+        const distributed = this.distribute(parenthesisCoefficient, simplifiedExpression)
+        return distributed
+    }
+
+    distribute(multiplier, expression) {
+        const distributed = this.splitExpression(expression)
+            .map((term, i) => {
+                let coefficient = this.parseCoefficient(term)
+                coefficient = coefficient * multiplier
+                const variable = this.parseVariable(term)
+                term = this.formatCoefficient(coefficient, variable, i)
+                return term
+            })
+            .join('')
+        return distributed
+    }
+
+    // Solve for variable
+    // Check espression with canSolve first
+    solve(variable, expression) {}
+
+    // check if there is only one occurance variable
+    canSolve(variable, expression) {}
+
+    parseTermSign(term) {
+        let sign = ''
+        if (/-/.test(term)) {
+            sign = '-'
+        }
+        return sign
+    }
+
+    parseTermValue(term) {
+        let value = 1
+        if (/\d/.test(term) === true) {
+            value = term.match(/\d+/)[0]
+        }
+        return value
+    }
+
+    parseCoefficient(term) {
+        let sign = this.parseTermSign(term)
+        let value = this.parseTermValue(term)
+        return parseInt(sign + value)
+    }
+
+    /**
+     * Split into an array of + and - terms, ie: [-x, 3y, z]
+     * @param {string} expression
+     */
+    splitExpression(expression) {
+        if (/(?!^)[-]/.test(expression) === true || /\+/.test(expression) === true) {
+            const keepSign = /(?=[-+])/g
+            return expression.split(keepSign) // (/(?!^)-|\+/)
+        }
+        console.log('Expression has only one term.')
+        return [expression]
+    }
+
+    /**
+     * returns an array of variables in the expression [2x, -3y, y] => [x, y]
+     * @param {array} expressionArray array returned by splitExpression
+     * This does not handle terms with multiple variables
+     */
+    getVariables(expressionArray) {
+        return expressionArray.map((term) => this.parseVariable(term)).filter(onlyUnique)
+    }
+
+    parseVariable(term) {
+        return term.match(/[a-zA-Z]{1}/)[0]
+    }
+
+    /**
+     * returns an array of two terms
+     * @param {string} expression simple expression like 2a+b
+     * @param {string} symbol \\+ for example.  Symbol that separates the terms
+     */
+    getTerms(expression, symbol) {
+        const termsRegex = new RegExp(`(.+)${symbol}(.+)`)
+        const termsMatch = expression.match(termsRegex)
+        const left = termsMatch[1]
+        const right = termsMatch[2]
+        return [left, right]
+    }
+
+    getCoefficient(term) {
+        let numberMatch = term.match(/\d+/)
+        const negative = /^-/.test(term)
+        let number = 1
+        if (numberMatch !== null) {
+            number = numberMatch[0]
+        }
+        return negative ? parseInt('-' + number) : parseInt(number)
+    }
 }
 
-const simplifier = new Simplifier([], "");
-const result = simplifier.add('2a-a+b');
-console.log(result);
+const simplifier = new Simplifier([], '')
+const result = simplifier.simplify('4(b+2(-2a-a))')
+console.log(result)
 
-module.exports = Simplifier;
+module.exports = Simplifier
--- simplifier.spec.js
+++ simplifier.spec.js
@@ -1,4 +1,4 @@
-const Simplifier = require("./simplifier");
+const Simplifier = require('./simplifier')
 
 let simplifier
 
@@ -7,36 +7,63 @@ describe('Simplifier', () => {
         simplifier = new Simplifier([], '')
     })
 
-    describe('add', () => {
-        
-        it('Should add terms without coefficients', () => {
-            const actual = simplifier.add('b+b')
-            expect(actual).toBe('2b')
-        })
+    // describe('add', () => {
 
-        it('Should add terms with coefficients', () => {
-            const actual = simplifier.add('2a+3a')
-            expect(actual).toBe('5a')
-        })
+    //     it('Should add terms without coefficients', () => {
+    //         const actual = simplifier.add('b+b')
+    //         expect(actual).toBe('2b')
+    //     })
 
-        it('Should add terms with one negative coefficient', () => {
-            const actual = simplifier.add('-2a+a')
-            expect(actual).toBe('-a')
-        })
+    //     it('Should add terms with coefficients', () => {
+    //         const actual = simplifier.add('2a+3a')
+    //         expect(actual).toBe('5a')
+    //     })
 
-        it('Should subtract terms without coefficients', () => {
-            const actual = simplifier.add('b-b')
-            expect(actual).toBe('')
-        })
+    //     it('Should add terms with one negative coefficient', () => {
+    //         const actual = simplifier.add('-2a+a')
+    //         expect(actual).toBe('-a')
+    //     })
 
-        it('Should subtract terms with coefficients', () => {
-            const actual = simplifier.add('2a-3a')
-            expect(actual).toBe('-a')
-        })
+    //     it('Should subtract terms without coefficients', () => {
+    //         const actual = simplifier.add('b-b')
+    //         expect(actual).toBe('')
+    //     })
 
-        it('Should subtract terms with one negative coefficient', () => {
-            const actual = simplifier.add('-2a-a')
+    //     it('Should subtract terms with coefficients', () => {
+    //         const actual = simplifier.add('2a-3a')
+    //         expect(actual).toBe('-a')
+    //     })
+
+    //     it('Should subtract terms with one negative coefficient', () => {
+    //         const actual = simplifier.add('-2a-a')
+    //         expect(actual).toBe('-3a')
+    //     })
+    // })
+
+    describe('simplify', () => {
+        it('Should simplify expressions with more than one term', () => {
+            const actual = simplifier.simplify('-2a-a')
             expect(actual).toBe('-3a')
+        })
+
+        it('Should simplify expressions with parenthesis', () => {
+            const actual = simplifier.simplify('(-2a-a)')
+            expect(actual).toBe('-3a')
+        })
+
+        // it('Should simplify expressions with a leading -', () => {
+        //     const actual = simplifier.simplify('-(-2a-a)')
+        //     expect(actual).toBe('-3a')
+        // })
+
+        it('Should simplify expressions with parenthesis and distribute a coeff to parenthesis', () => {
+            const actual = simplifier.simplify('2(-2a-a)')
+            expect(actual).toBe('-6a')
+        })
+
+        it('Should simplify expressions with multiple parenthesis.', () => {
+            const actual = simplifier.simplify('4(b+2(-2a-a))')
+            expect(actual).toBe('4b-24a')
         })
     })
 })
